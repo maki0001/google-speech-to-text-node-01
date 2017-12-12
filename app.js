46d7bf0eac3c8f46092bd1a0e96191a28af903a5
牧内 信吾 <J32200@jbcc.co.jp>
2017-12-12 12:45:21 +0900

    add api call

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var speechToText = require('./routes/speech-to-text');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/speech-to-text', speechToText);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/speech-to-text.js
@@ -0,0 +1,112 @@
+//
+// var start = function(){
+//   console.log('start');
+//
+//   var socket;
+//   (function(){
+//     // socket通信が開始される
+//     socket = io.connect("http://localhost:3000");
+//   })();
+// }
+//
+// var stop = function(){
+//   console.log('stop');
+// }
+
+//画面表示時に実行される
+// staticブロック的な動き？
+var socket, emit;
+(function(){
+    socket = io.connect("http://localhost:3000");
+    emit = function (name, data){
+        // json → 文字列に変換して送信する関数
+        socket.emit(name, JSON.stringify(data));
+    }
+})();
+
+//メイン
+var ctx;
+window.onload = function(){
+    var startBtn = document.getElementById('start');
+    var stopBtn = document.getElementById('stop');
+    var message = document.getElementById('message');
+
+    //TODO マイクの起動と音声データの送信
+    startBtn.addEventListener("click", function(){
+        console.log('start');
+
+        // マイク起動
+        ctx = new AudioContext();
+        // マイク取る
+        navigator.getUserMedia(
+          { audio: true },
+          function(stream) {
+            // AudioNodeに
+            var source = ctx.createMediaStreamSource(stream);
+
+            // オーディオのサンプルに触るなら
+            var bufferSize = 4096;
+            var processor = ctx.createScriptProcessor(bufferSize, 1, 1);
+            processor.onaudioprocess = function(ev) {
+              // console.log('onaudioprocess');
+              var inputBuffer  = ev.inputBuffer;
+              var outputBuffer = ev.outputBuffer;
+
+              var inputData  = inputBuffer.getChannelData(0);
+              var outputData = outputBuffer.getChannelData(0);
+
+              //何もしなくてもoutputは返さないと音が出ない
+              outputData.set(inputData);
+              //音声加工があればここに書く
+
+              socket.emit('audio', outputData.buffer);
+
+              // // Float32なArrayBuffer
+              // var inputData  = inputBuffer.getChannelData(0);
+              // var outputData = outputBuffer.getChannelData(0);
+              //
+            };
+
+            var biquadFilter = ctx.createBiquadFilter();
+            biquadFilter.type = 'bandpass';
+            biquadFilter.frequency.value = (100 + 7000) / 2;
+            biquadFilter.Q.value = 0.25;
+
+            // source.connect(processor);
+            // processor.connect(ctx.destination);
+            source.connect(processor);
+            processor.connect(biquadFilter);
+            biquadFilter.connect(ctx.destination);
+          },
+          function(err) {
+            console.error(err);
+          }
+        );
+
+        // emit('message', {text: message.value});
+    });
+
+    // socket.on('receiveMessage', function(d){
+    //     var data = JSON.parse(d), // 文字列→JSON
+    //         li = document.createElement('li'), // liタグ作成
+    //         list = document.getElementById('messageList'); // ulタグの取得
+    //     li.textContent = data.text; // liタグに値を入れる
+    //     list.appendChild(li); // ulタグの子ノードとして作成したliタグを追加
+    // });
+
+    //TODO 音声の変換結果を取得する
+    socket.on('receiveTranscribedMessage', d => {
+        console.log('on receiveTranscribedMessage');
+        var data = JSON.parse(d), // 文字列→JSON
+            li = document.createElement('li'), // liタグ作成
+            list = document.getElementById('messageList'); // ulタグの取得
+        li.textContent = data.text; // liタグに値を入れる
+        list.appendChild(li); // ulタグの子ノードとして作成したliタグを追加
+    });
+
+    //TODO マイク停止
+    stopBtn.addEventListener("click", function(){
+      // var ctx = new AudioContext();
+      ctx.close();
+    });
+};
